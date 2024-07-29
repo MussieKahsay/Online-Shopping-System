d5ee16062160c5e580f5262494cb92d9fa4825af
nahom <nbirhane@gmail.com>
2024-07-28 21:59:23 -0500

    Some Classes Added

---
+++ src/main/java/edu/miu/cs/cs425/model/Product.java
@@ -0,0 +1,5 @@
+package edu.miu.cs.cs425.model;
+
+public class Product {
+
+}
---
+++ src/main/java/edu/miu/cs/cs425/service/ProductServiceImpl.java
@@ -0,0 +1,8 @@
+package edu.miu.cs.cs425.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl {
+
+}
