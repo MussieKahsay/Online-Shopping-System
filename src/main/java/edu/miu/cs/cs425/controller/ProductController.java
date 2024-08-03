630e6fea47902442f1ac68916426c5a42153b40f
michal tesfu <michaltesfu2@gmail.com>
2024-08-03 16:51:00 -0500

    first commit

--- src/main/java/edu/miu/cs/cs425/controller/ProductController.java
+++ src/main/java/edu/miu/cs/cs425/controller/ProductController.java
@@ -1,7 +1,69 @@
 package edu.miu.cs.cs425.controller;
+import edu.miu.cs.cs425.model.Product;
+import edu.miu.cs.cs425.service.ProductService;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
+@RequestMapping("/ecom/products")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductService productService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Product> addProduct(@Valid @RequestBody Product product) {
+        Product newProduct = productService.addProduct(product);
+        return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{productId}")
+    public ResponseEntity<Product> updateProduct( @PathVariable Integer productId, @Valid @RequestBody ProductDTO updatedProduct) {
+        Product updatedProductResult = productService.updateProduct(productId, updatedProduct);
+        return new ResponseEntity<>(updatedProductResult, HttpStatus.OK);
+    }
+
+    @GetMapping("/product-By-name/{name}")
+    public ResponseEntity<List<Product>> getProductByName(@PathVariable String name) {
+        List<Product> products = productService.getProductByName(name);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Product>> search(
+            @RequestParam (required = false)  String keyword,
+            @RequestParam(required = false,defaultValue = "asc") String sort,
+            @RequestParam(required = false,defaultValue = "price") String sortBy
+
+    ) {
+        List<Product> products = productService.getAllProduct(keyword, sort, sortBy);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Product>> getProductByCategory(@PathVariable String category) {
+        List<Product> products = productService.getProductByCategory(category);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getSingleProduct(@PathVariable Integer productId) {
+        Product singleProsuct = productService.getSingleProduct(productId);
+        return new ResponseEntity<>(singleProsuct, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<String> removeProduct(@PathVariable Integer productId) {
+        productService.removeProduct(productId);
+        return new ResponseEntity<>("Product removed successfully.", HttpStatus.OK);
+    }
 }
---
+++ src/main/java/edu/miu/cs/cs425/serviceImpl/ReviewServiceImpl.java
@@ -0,0 +1,74 @@
+package edu.miu.cs.cs425.serviceImpl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import edu.miu.cs.cs425.exception.ReviewException;
+import edu.miu.cs.cs425.model.Product;
+import edu.miu.cs.cs425.model.Review;
+import edu.miu.cs.cs425.repository.ProductRepository;
+import edu.miu.cs.cs425.repository.ReviewRepository;
+import edu.miu.cs.cs425.service.ReviewService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewServiceImpl implements ReviewService {
+
+    private final ProductRepository productRepository;
+
+    private final ReviewRepository reviewRepository;
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public Review addReviewToProduct(Integer productId, Integer userId, Review review) throws ReviewException {
+        Product existingProduct = productRepository.findById(productId)
+                .orElseThrow(() -> new ReviewException("Product Not Found"));
+
+        User existingUser = userRepository.findById(userId)
+                .orElseThrow(() -> new ReviewException("User Not Found In Database"));
+
+        review.setUser(existingUser);
+        review.setProduct(existingProduct);
+        review.setCreatedAt(LocalDateTime.now());
+
+        return reviewRepository.save(review);
+    }
+
+    @Override
+    public Review updateReviewToProduct(Integer reviewId, Review review) throws ReviewException {
+        Review existingReview = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ReviewException("Review With Id "+reviewId+"Not Found In DataBase"));
+
+        existingReview.setComment(review.getComment());
+        existingReview.setRating(review.getRating());
+        return existingReview;
+    }
+
+    @Override
+    public void deleteReview(Integer reviewId) throws ReviewException {
+        Review existingReview = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ReviewException("Review With Id "+reviewId+"Not Found In DataBase"));
+
+        reviewRepository.delete(existingReview);
+
+    }
+
+    @Override
+    public List<Review> getAllReviewOfProduct(Integer productId) throws ReviewException {
+        Product existingProduct = productRepository.findById(productId)
+                .orElseThrow(() -> new ReviewException("Invalid Product id"));
+
+        List<Review> allReviewsByProductId = reviewRepository.findAllReviewsByProductId(productId);
+        if(allReviewsByProductId.isEmpty()) {
+            throw new ReviewException ("No Rewiew Of Given Product is Available");
+        }
+        return allReviewsByProductId;
+    }
+
+}
